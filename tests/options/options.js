af6d3ece8fd962d04896f224dd640e47ea1a152c
Alexandre <alexandre_mariano@hotmail.com.br>
2025-11-25 11:56:19 -0300

    atualizando a pipeline e as options para receber valores no input do github

--- tests/options/options.js
+++ tests/options/options.js
@@ -1,13 +1,24 @@
+import { __ENV } from 'k6';
+
 export function accountOptions() {
+    const vu_1 = parseInt(__ENV.K6_VUS_1) || 100;
+    const duration_1 = __ENV.K6_DURATION_1 || '30s';
+
+    const vu_2 = parseInt(__ENV.K6_VUS_2) || 150;
+    const duration_2 = __ENV.K6_DURATION_2 || '50s';
+    
+    const vu_3 = parseInt(__ENV.K6_VUS_3) || 100;
+    const duration_3 = __ENV.K6_DURATION_3 || '30s';
+    
     return {
         stages:[
-            {duration: '20s', target: 100},
-            {duration: '30s', target: 120},
-            {duration: '15s', target: 0},
+            {duration: duration_1, target: vu_1},
+            {duration: duration_2, target: vu_2},
+            {duration: duration_3, target: vu_3},
         ],
         thresholds: {   
-            http_req_duration: ['p(95)<200'], // 95% das requisições devem responder em até 1.5 segundos
-            http_req_failed: ['rate<0.01'], // 1% das requisiçoes podem ocorrer erro
+            http_req_duration: ['p(95)<200'], // 95% das requisições devem responder em até 200ms
+            http_req_failed: ['rate<0.01'], // 1% das requisições podem ocorrer erro
         }
     }
 }
