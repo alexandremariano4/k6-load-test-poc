bbb2430b020eb35104847839283b76c03931f021
Alexandre <alexandre_mariano@hotmail.com.br>
2025-11-25 11:11:17 -0300

    alterando o htmlReport e textSummary para código local

--- tests/testCi.js
+++ tests/testCi.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js'
-import { textSummary } from 'https://jslib.k6.io/k6-summary/0.1.0/index.js'
+import { htmlReport     } from './report/htmlReport.js';
+import { textSummary    } from './report/textSummary.js'
 
 
 export default function (){
